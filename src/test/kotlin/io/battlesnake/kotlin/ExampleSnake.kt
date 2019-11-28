564ae29e2736021a16b87fc052291f2bca22cd5d
Paul Ambrose <pambrose@mac.com>
2019-11-27 23:44:53 -0800

    0.2.10
    * Update jars

--- src/test/kotlin/io/battlesnake/kotlin/ExampleSnake.kt
+++ src/test/kotlin/io/battlesnake/kotlin/ExampleSnake.kt
@@ -7,28 +7,28 @@ import io.battlesnake.kotlin.ExampleSnake.GameContext
 
 object ExampleSnake : AbstractBattleSnake<GameContext>() {
 
-    // Add any necessary snake-specific data to GameContext class
-    class GameContext : AbstractGameContext()
+  // Add any necessary snake-specific data to GameContext class
+  class GameContext : AbstractGameContext()
 
-    // Called at the beginning of each game on Start
-    override fun gameContext(): GameContext = GameContext()
+  // Called at the beginning of each game on Start
+  override fun gameContext(): GameContext = GameContext()
 
-    override fun gameStrategy(): Strategy<GameContext> =
-        strategy(true) {
+  override fun gameStrategy(): Strategy<GameContext> =
+    strategy(true) {
 
-            // StartResponse describes snake color and head/tail type
-            onStart { context: GameContext, request: StartRequest ->
-                StartResponse("#ff00ff", "beluga", "bolt")
-            }
+      // StartResponse describes snake color and head/tail type
+      onStart { context: GameContext, request: StartRequest ->
+        StartResponse("#ff00ff", "beluga", "bolt")
+      }
 
-            // MoveResponse can be LEFT, RIGHT, UP or DOWN
-            onMove { context: GameContext, request: MoveRequest ->
-                RIGHT
-            }
-        }
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-        run()
+      // MoveResponse can be LEFT, RIGHT, UP or DOWN
+      onMove { context: GameContext, request: MoveRequest ->
+        RIGHT
+      }
     }
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    run()
+  }
 }
